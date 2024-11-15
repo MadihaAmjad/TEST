3e8f2c160ac2664e321e6fcc5bd7ae1861eb111b
MadihaAmjad <madihaamjad06@gmail.com>
2024-11-15 17:03:46 +0500

    Again update the commit

--- weth.sol
+++ weth.sol
@@ -1,20 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >0.5.0;
 
-contract tos1{
-    string public name     = "tos1";
-    string public symbol   = "tos1"; 
-    uint8  public decimals = 18;
-     uint256 public totalsupply;
+contract WETH {
+    //Only check file
 
-    event  Approval(address indexed src, address indexed guy, uint wad);
-    event  Transfer(address indexed src, address indexed dst, uint wad);
-    event  Deposit(address indexed dst, uint wad);
-    event  Withdrawal(address indexed src, uint wad);
-
-    mapping (address => uint)                       public  balanceOf;
-    mapping (address => mapping (address => uint))  public  allowance;
-
-  
 }
-
